b5931f242cde40a4a310b581fa775b580cf6065c
hga50 <himank_goel@sfu.ca>
2022-04-01 00:11:10 +0530

    added nms logic

--- eval.py
+++ eval.py
@@ -69,7 +69,14 @@ if not args.test:
         picked_ids = []
         
         for imgg in range(len(pred_confidence_)):
-            picked_ids.append(non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.8))
+            p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.8)
+            if len(p_ids) == 0:
+                p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.6)
+            if len(p_ids) == 0:
+                p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.5)
+            
+            picked_ids.append(p_ids)
+            
             out_list = []
             for s_id in picked_ids[-1]:
                 gx, gy, gw, gh = recover_gt_bbox(pred_box_[imgg, :, :], boxs_default, s_id)
@@ -127,7 +134,13 @@ else:
         picked_ids = []
         
         for imgg in range(len(pred_confidence_)):
-            picked_ids.append(non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.8))
+            p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.8)
+            if len(p_ids) == 0:
+                p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.6)
+            if len(p_ids) == 0:
+                p_ids = non_maximum_suppression(pred_confidence_[imgg], pred_box_[imgg], boxs_default, 3, 0.1, 0.5)
+            
+            picked_ids.append(p_ids)
             out_list = []
             for s_id in picked_ids[-1]:
                 gx, gy, gw, gh = recover_gt_bbox(pred_box_[imgg, :, :], boxs_default, s_id)
